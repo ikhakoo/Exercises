4321513117e5953cdbd3610028bc0cd87851ddc7
ikhakoo <ikhakoo@gmail.com>
2015-03-31 15:12:29 -0400

    Exercise 2 Part 2

--- exercise2.rb
+++ exercise2.rb
@@ -1,18 +1,24 @@
-# Part 1 - Tip Calculator
+# # Part 1 - Tip Calculator
 
-puts "Leave a Good Tip!!"
-puts "How much was your bill?"
-bill_amount = gets.chomp
+# puts "Leave a Good Tip!!"
+# puts "How much was your bill?"
+# bill_amount = gets.chomp
 
-puts "How many percent would you like to tip?"
-tip_percentage = gets.chomp
+# puts "How many percent would you like to tip?"
+# tip_percentage = gets.chomp
 
-tip_value = bill_amount.to_f/100 * tip_percentage.to_i
+# tip_value = bill_amount.to_f/100 * tip_percentage.to_i
 
-total_with_tip = bill_amount.to_i + tip_value.to_i
-puts "Your total bill pre-tip is: $" + "#{bill_amount}"
-puts "Your total tip is: $" + "#{tip_value}"
-puts "Your total bill with tip is $" + "#{total_with_tip}"
+# total_with_tip = bill_amount.to_i + tip_value.to_i
+# puts "Your total bill pre-tip is: $" + "#{bill_amount}"
+# puts "Your total tip is: $" + "#{tip_value}"
+# puts "Your total bill with tip is $" + "#{total_with_tip}"
 
+Part 2 - Integer to String
 
+x = 7
+y = "7"
+sum_of_xy = x + y.to_i
+
+puts sum_of_xy
 
