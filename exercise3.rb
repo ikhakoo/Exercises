99ada1affb21da880af789c5109ded595622eed7
ikhakoo <ikhakoo@gmail.com>
2015-03-31 15:40:07 -0400

    exercise 3 working version with birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 puts "What is your name?"
 name = gets.chomp
-puts "Hi #{name}!"
+
+puts "How old are you?"
+age = gets.chomp
+
+time = Time.new
+
+birth_year = time.year - age.to_i
+puts "Hi #{name}! You were born in #{birth_year}"
